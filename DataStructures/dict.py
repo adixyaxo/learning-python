fa58f86bc54778c5ed48f488d6a5d3e43dc618ec
adixyxo <aditya0dagar@gmail.com>
2026-01-01 14:55:09 +0530

    learning dict comprehension

--- DataStructures/dict.py
+++ DataStructures/dict.py
@@ -24,3 +24,34 @@ marks["Ankit"] = 88  # Adding a new key-value pair
 marks["Rahul"] = 95  # Updating an existing key's value
 print(marks)
 
+"--------------------------------"
+
+# Dictionary methods
+marks.keys()  # Returns a view object of all keys
+marks.values()  # Returns a view object of all values
+marks.items()  # Returns a view object of key-value pairs
+marks.pop("Karan")  # Removes the key-value pair with the specified key
+print(marks)
+marks.clear()  # Removes all key-value pairs from the dictionary
+print(marks)
+
+
+"--------------------------------"
+# How to print dictionary in a formatted way or pretty way or in a table way
+import pprint
+marks = {
+    "Aditya": 85,
+    "Rahul": 92,
+    "Priya": 78,
+    "Karan": 95
+}
+pprint.pprint(marks)
+pprint.pprint(marks, width=1)  # print each key-value pair in a new line
+"--------------------------------"
+
+
+# Dictionary comprehension
+
+# dict of numbers and table of 5
+table_of_5 = {i: i*5 for i in range(1, 11)}
+print(table_of_5)
