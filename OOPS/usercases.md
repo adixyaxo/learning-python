c93fb2fe057b607213fcc7e275e865abb6174317
adixyxo <aditya0dagar@gmail.com>
2026-01-06 18:38:29 +0530

    usecases notes

---
+++ OOPS/usercases.md
@@ -0,0 +1,6 @@
+# Summary Use Cases
+1. Use Inheritance when you have different types of the same thing (e.g., CreditCard and DebitCard are both Cards).
+
+1. Use Nesting (rarely) only when a class is a tiny helper strictly for one other class and never used elsewhere.
+
+1. Use Composition (Separate classes) when one object consists of another (e.g., A Bank has Customers; an Account has Transactions).
--- advancepythonconcepts/practice_set.py
+++ advancepythonconcepts/practice_set.py
@@ -1,7 +1,35 @@
-def logger(func):
-    print("function is going to be called :")
-    func()
+def logger(a):
+    def wrapper():
+        print("function is going to be called :")
+        a()
+    return wrapper
     
 @logger
 def say_hello():
     print("Hello")
+    
+say_hello()
+
+print("\n---\n")
+
+from time import time
+
+
+def timer(func):
+    def wrapper(n):
+        t1 = time()
+        result = func(n)
+        t2 = time()
+        print ((t2)-(t1))
+        return result
+    return wrapper
+
+
+@timer
+def sum(n):
+    a = 0
+    for i in range(1,n+1):
+        a+=i
+    return a
+
+print(x := sum(1000000))
