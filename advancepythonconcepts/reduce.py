dfa042353448cdb4ce250ea2f6371277d4e44f38
adixyxo <aditya0dagar@gmail.com>
2026-01-06 13:52:50 +0530

    learning reduce

---
+++ advancepythonconcepts/reduce.py
@@ -0,0 +1,5 @@
+# map filter and reduce are higher order functions in python
+# they operate on iterables like lists, tuples, etc. 
+# they provide a concise and functional way to process data and perform common operations on sequences of data without using explicit loops.
+
+numbers = [1, 2, 3, 4, 5, 6 , 21, 45]
