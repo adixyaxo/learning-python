5bec1e3a9425db754350921748c3fa103d83fcf4
adixyxo <aditya0dagar@gmail.com>
2026-01-08 05:03:42 +0530

    argparse library or module expored

--- Fileio/commandlineulits.py
+++ Fileio/commandlineulits.py
@@ -2,11 +2,25 @@ import argparse
 
 parser = argparse.ArgumentParser(description='simple calculator')
 
-parser.add_argument("num1",type = float(), help="second number")
-parser.add_argument("num2",type = float(), help="first number")
+parser.add_argument("num1",type = float, help="first number")
+parser.add_argument("num2",type = float, help="second number")
 
-parser.add_argument("operation"choices= ["add","sub","div","mul"] help = "operations to perform")
+parser.add_argument("operation",choices= ["add","sub","div","mul"], help = "operations to perform" )
 
 args = parser.parse_args()
 
 print(args)
+
+if (args.operation == "add"):
+    print(f"{args.num1+args.num2}")
+elif (args.operation == "sub"):
+    print(f"{args.num1-args.num2}")
+elif (args.operation == "div"):
+    try:
+        print(f"{args.num1/args.num2}")
+    except ZeroDivisionError as e :
+        print("You cannot divide by zero")
+elif (args.operation == "mul"):
+    print(f"{args.num1*args.num2}")
+else :
+    print("wrong choice enter again")
