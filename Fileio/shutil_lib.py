1289fc8d83920f9d8b57621dbac0fa71d4633972
adixyxo <aditya0dagar@gmail.com>
2026-01-07 18:58:36 +0530

    shutil module

---
+++ Fileio/shutil_lib.py
@@ -0,0 +1,9 @@
+import shutil
+# learn more in shutil documentation and explore more 
+
+# shutil.rmtree() # can remove any directory doesnt matter empty of illed 
+# be carefull when running this programme
+
+shutil.copy("jhon_doe","jhon_doe_ki_copy")
+
+shutil.move("Jhon_doe_ki_copy", "dir/")
