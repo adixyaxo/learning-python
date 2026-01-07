5b83eece5c0745bf3a7126ba7cfec06484054b41
adixyxo <aditya0dagar@gmail.com>
2026-01-07 18:03:39 +0530

    reading files

--- Fileio/fileio.py
+++ Fileio/fileio.py
@@ -1,3 +1,12 @@
 # in python we can read a file write to a file and can also append in a file
 
+# IMPORTANT NOTE DO cd ./folder_name or name sure you are in right directry in terminal with the files to run this programme 
+# cd .\Fileio\
 
+a_file = open("aditya.txt","r") # if we want to read txt files we write rt and if we want to read binary files we write rb
+
+content = a_file.read()
+
+print(content)
+
+a_file.close()
